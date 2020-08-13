21454b046c8c52ff5c89331bc80862ab43b5ce17
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-08-12 23:39:30 -0500

    added post route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { nodemon } = require('nodemon');
+const inputCheck = require('./utils/inputCheck');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -24,17 +25,17 @@ app.get('/api/candidates', (req, res) => {
     const sql = `SELECT * FROM candidates`;
     const params = [];
     db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: rows
-      });
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
     });
-  });
+});
 
 
 // Get single candidate
@@ -43,72 +44,83 @@ app.get('/api/candidate/:id', (req, res) => {
                  WHERE id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: row
-      });
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
     });
-  });
+});
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
-  const sql = `DELETE FROM candidates WHERE id = ?`;
-  const params = [req.params.id];
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      return;
-    }
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
 
-    res.json({
-      message: 'successfully deleted',
-      changes: this.changes
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
     });
-  });
 });
 
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Default response for any other request (NOT FOUND) Catch all
-app.use((req, res) => {
-    res.status(404).end();
-});
-
-
-
-
-
-db.on('open', () => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+    //Default response for any other request (NOT FOUND) Catch all
+    app.use((req, res) => {
+        res.status(404).end();
+    });
+
+
+
+
+
+    db.on('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    });
 
